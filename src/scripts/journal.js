7e66e3ebd2fe6b1d36b46b5340fab55941c69e99
Sarah Fleming <sarah.k.fleming@gmail.com>
2019-07-12 15:41:41 -0500

    Pushing current before starting Part 3

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -21,7 +21,7 @@ const journalEntries = [
         entry: 'Today we covered Book 2, Chapter 1 "Fun with Manipulating DOM." We practiced using document.querySelector() and document.querySelectorAll() to target HTML elements in order to add and/or remove classes from them.',
         mood: "OK"
     }
-        {
+    {
         date: "07/10/2019",
         concepts: "Celebrity Tribute Group Project: Practice using Git, GitHub, and Flexbox",
         entry: 'Today we completed and presented our first group projects. My group did a celebrity tribute page for Rihanna. We had to practice pushing and merging on GitHub as well as CSS styling using Flexbox.',
