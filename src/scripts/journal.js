b5f16155f7f98ad73e4949e6ab1f3ab656facf09
Sarah Fleming <sarah.k.fleming@gmail.com>
2019-07-14 20:39:41 -0500

    Pushed existing journal entries into array

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -1,30 +1,27 @@
-/*
-    Define the keys and value for a JavaScript object that
-    represents a journal entry about what you learned today
-*/
-const journalEntries = [
-    {
-        date: "07/12/2019",
-        conceptsCovered: "JavaScript Objects",
-        entry: 'Today we covered Book 2, Chapter 2 "JavaScript Objects." We practiced accessing values within various combinations of objects and arrays.',
-        mood: "OK"
-    }
-    {
-        date: "07/12/2019",
-        concepts: "JavaScript Objects",
-        entry: 'Today we covered Book 2, Chapter 2 "JavaScript Objects." We practiced accessing values within various combinations of objects and arrays.',
-        mood: "OK"
-    }
-    {
-        date: "07/11/2019",
-        concepts: "DOM Manipulation with JavaScript",
-        entry: 'Today we covered Book 2, Chapter 1 "Fun with Manipulating DOM." We practiced using document.querySelector() and document.querySelectorAll() to target HTML elements in order to add and/or remove classes from them.',
-        mood: "OK"
-    }
-    {
-        date: "07/10/2019",
-        concepts: "Celebrity Tribute Group Project: Practice using Git, GitHub, and Flexbox",
-        entry: 'Today we completed and presented our first group projects. My group did a celebrity tribute page for Rihanna. We had to practice pushing and merging on GitHub as well as CSS styling using Flexbox.',
-        mood: "OK"
-    }
-]
+const journalEntries = [];
+
+const journalEntryJSObjects =
+{
+    date: "07/12/2019",
+    concepts: "JavaScript Objects",
+    entry: 'Today we covered Book 2, Chapter 2 "JavaScript Objects." We practiced accessing values within various combinations of objects and arrays.',
+    mood: "OK"
+}
+
+const journalEntryDOM =
+{
+    date: "07/11/2019",
+    concepts: "DOM Manipulation with JavaScript",
+    entry: 'Today we covered Book 2, Chapter 1 "Fun with Manipulating DOM." We practiced using document.querySelector() and document.querySelectorAll() to target HTML elements in order to add and/or remove classes from them.',
+    mood: "OK"
+}
+
+const journalEntryGit =
+{
+    date: "07/10/2019",
+    concepts: "Celebrity Tribute Group Project: Practice using Git, GitHub, and Flexbox",
+    entry: 'Today we completed and presented our first group projects. My group did a celebrity tribute page for Rihanna. We had to practice pushing and merging on GitHub as well as CSS styling using Flexbox.',
+    mood: "OK"
+}
+
+journalEntries.push(journalEntryJSObjects, journalEntryDOM, journalEntryGit);
