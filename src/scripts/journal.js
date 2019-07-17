69e357b24a516df7ee02add135ed49fa29aff5d9
Sarah Fleming <sarah.k.fleming@gmail.com>
2019-07-17 09:03:28 -0500

    Added another journal entry

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -1,10 +1,26 @@
 const journalEntries = [];
 
+const journalEntryJSFunctions2 =
+{
+    date: "07/16/2019",
+    concepts: "JavaScript Functions",
+    entry: "Today we had lab time most of the day to work on exercises related to JavaScript functions. I practiced for...in loops and the forEach method.",
+    mood: "OK"
+}
+
+const journalEntryJSFunctions =
+{
+    date: "07/15/2019",
+    concepts: "JavaScript Functions",
+    entry: "Today we covered Book 2, Chapter 4 "Functions & Logic." We practiced writing functions that involved iterations through arrays and objects",
+    mood: "OK"
+}
+
 const journalEntryJSObjects =
 {
     date: "07/12/2019",
     concepts: "JavaScript Objects",
-    entry: 'Today we covered Book 2, Chapter 2 "JavaScript Objects." We practiced accessing values within various combinations of objects and arrays.',
+    entry: "Today we covered Book 2, Chapter 2 "JavaScript Objects." We practiced accessing values within various combinations of objects and arrays.",
     mood: "OK"
 }
 
@@ -12,7 +28,7 @@ const journalEntryDOM =
 {
     date: "07/11/2019",
     concepts: "DOM Manipulation with JavaScript",
-    entry: 'Today we covered Book 2, Chapter 1 "Fun with Manipulating DOM." We practiced using document.querySelector() and document.querySelectorAll() to target HTML elements in order to add and/or remove classes from them.',
+    entry: "Today we covered Book 2, Chapter 1 "Fun with Manipulating DOM." We practiced using document.querySelector() and document.querySelectorAll() to target HTML elements in order to add and/or remove classes from them.",
     mood: "OK"
 }
 
@@ -20,7 +36,7 @@ const journalEntryGit =
 {
     date: "07/10/2019",
     concepts: "Celebrity Tribute Group Project: Practice using Git, GitHub, and Flexbox",
-    entry: 'Today we completed and presented our first group projects. My group did a celebrity tribute page for Rihanna. We had to practice pushing and merging on GitHub as well as CSS styling using Flexbox.',
+    entry: "Today we completed and presented our first group projects. My group did a celebrity tribute page for Rihanna. We had to practice pushing and merging on GitHub as well as CSS styling using Flexbox.",
     mood: "OK"
 }
 
