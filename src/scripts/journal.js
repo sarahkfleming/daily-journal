a4f3a7e870d9e53375232176cc0ec8c7ca04671c
Sarah Fleming <sarah.k.fleming@gmail.com>
2019-07-24 14:03:38 -0500

    finished modularization of code

---
+++ src/scripts/journal.js
@@ -0,0 +1,10 @@
+/*
+    Main application logic that uses the functions and objects
+    defined in the other JavaScript files.
+
+    Change the fake variable names below to what they should be
+    to get the data and display it.
+
+    objectWithGetterMethod.methodToGetData().then(functionThatRendersData)
+*/
+API.getJournalEntries().then(renderJournalEntries)
