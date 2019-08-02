d2267ae5f8aac59a303f3b13e4a00cb80dd1c6e4
Sarah Fleming <sarah.k.fleming@gmail.com>
2019-08-02 15:43:33 -0500

    took user inputted journal entry and displayed it in the DOM

--- src/scripts/data.js
+++ src/scripts/data.js
@@ -1,7 +1,16 @@
 // Get the data
 const API = {
-    getJournalEntries () {
+    getJournalEntries() {
         return fetch("http://localhost:3000/entries")
             .then(response => response.json())
+    },
+    saveJournalEntry(newJournalEntry) {
+        return fetch("http://localhost:3000/entries", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(newJournalEntry),
+        })
     }
 }
--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -7,6 +7,8 @@
 
     objectWithGetterMethod.methodToGetData().then(functionThatRendersData)
 */
+
+// Get journal entries from JSON file then render them in the DOM
 API.getJournalEntries().then(entries => { entriesDOM.renderJournalEntries(entries)})
 
 // Get reference to Record Journal Entry button
@@ -18,8 +20,33 @@ const getConcepts = document.querySelector("#conceptsCovered")
 const getEntry = document.querySelector("#journalEntry")
 const getMood = document.querySelector("#mood")
 
+// Factory function to create an object
+const newJournalEntry = (date, concepts, entry, mood) => {
+    return {
+        "date": date.value,
+        "concepts": concepts.value,
+        "entry": entry.value,
+        "mood": mood.value,
+        }
+}
+
+// Submission button event listener
 submitJournalEntry.addEventListener('click', () => {
-    let practice = getDate.value
-    console.log(practice)
+    const createOneEntry = newJournalEntry(getDate,
+        getConcepts,
+        getEntry,
+        getMood)
+    // Post new journal entry then render it in the DOM
+    API.saveJournalEntry(createOneEntry)
+        .then(API.getJournalEntries)
+        .then(entries => {
+            entriesDOM.renderJournalEntries(entries)
+        })
+        .then( () => {
+            getDate.value = ""
+            getConcepts.value = ""
+            getEntry.value = ""
+            getMood.value = ""
+        })
 })
 
